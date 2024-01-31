d3d0a520a6675053ae0fd6051c2bb796107bd94b
Panjete <suparmaansingh@gmail.com>
2024-01-31 12:42:17 +0530

    added other flavors of parallelism

--- omp_LU.c
+++ omp_LU.c
@@ -20,29 +20,33 @@ Components parallelised :-
 
 
 
-To do : Test for n=8000
+To do : Test for n=8000 - too big, currently does not scale well enough
 
 
 */
 
 void LU(int n, int t); /* Wrapper function for the decomposition */
 
+/* Initialisers */
 void init_a(int n, double** m); /* Initialising the matrix with random values */
 void init_l(int n, double** m); /* Initialising the output L with lower triangular values */
 void init_u(int n, double** m); /* Initialising the output U with upper triangular values */
-void init_pi(int n, int* m); /* Initialising the output pi with linear ints */
-void init_pa(int n, double** m1, double** m2); /* Storing original A as pa, for verification of convergence later */
+void init_pi(int n, int* m, int t); /* Initialising the output pi with linear ints */
+void init_pa(int n, double** m1, double** m2, int t); /* Storing original A as pa, for verification of convergence later */
 
+/* LU Decomposer Modules */
 int find_max(double** a, int n, int k);
-void swap_row(double** m, int r1, int r2, int k_upto); /* Swap R1 with R2, upto column k*/
-void upd_UL(double** a, int n, int k, double** u, double** l); /* Fill up the u & l matrices after the global kth iteration */
-void upd_A(double** a, int n, int k, double** u, double** l); /* Update a now that it's kth row has been processed */
-
+void swap_row(double** m, int r1, int r2, int k_upto, int t); /* Swap R1 with R2, upto column k*/
+void swap_row_v2(double* m1, double* m2, int k_upto); /* Swap R1 with R2, upto column k*/
+void upd_UL(double** a, int n, int k, double** u, double** l, int t); /* Fill up the u & l matrices after the global kth iteration */
+void upd_UL_v2(double** a, int n, int k, double** u, double** l, int t); /* Fill up the u & l matrices after the global kth iteration */
+void upd_A(double** a, int n, int k, double** u, double** l, int t); /* Update a now that it's kth row has been processed */
+void upd_A_v2(double** a, int n, int k, double** u, double** l, int t); /* Update a now that it's kth row has been processed */
 
 /* Funtions for verification of Convergence via L2,1 norm */
-void mat_mult(double** m1, double** m2, double** m3, int n); // compute L*U and save in a_prime
+void mat_mult(double** m1, double** m2, double** m3, int n, int t); // compute L*U and save in a_prime
 void mat_rearrange(double** m, int* pi, int n); // compute PA from a and pi and save in pa
-void mat_sub(double** m1, double** m2, int n); // subtract pa - a_prime and save in pa
+void mat_sub(double** m1, double** m2, int n, int t); // subtract pa - a_prime and save in pa
 void mat_print(double** m, int n); // For Debugging and manual verif
 double L2c1(double** m, int n); // computing the L2,1 norm of the residual matrix
 
@@ -50,6 +54,11 @@ int main(int argc, char* argv[]) {
 
     int n = strtol(argv[1], NULL, 10); // User input
     int t = strtol(argv[2], NULL, 10); // User input
+
+    if(t == 0){
+        printf("Please pass non-zero number of threads!\n");
+        return 0;
+    }
 
     LU(n, t); // Pass control to the LU-decomposer
 
@@ -72,41 +81,30 @@ void LU(int n, int t){
         a_prime[i] = (double*)malloc(n * sizeof(double));
     }
 
-#pragma parallel num_threads(t)
+#pragma omp parallel num_threads(t)
 {
     init_a(n, a);
 }
-#pragma parallel num_threads(t)
+#pragma omp parallel num_threads(t)
 {
     init_l(n, l);
 }
-#pragma parallel num_threads(t)
+#pragma omp parallel num_threads(t)
 {
     init_u(n, u);
 }
-    init_pi(n, pi);    // Internally Parallelised
-    init_pa(n, a, pa); // Internally Parallelised
-    
-    // printf("Matrix A at the start = \n");
-    // mat_print(a, n);
-
-    // printf("Matrix L at the start = \n");
-    // mat_print(l, n);
-
-    // printf("Matrix U at the start = \n");
-    // mat_print(u, n);
+    init_pi(n, pi, t);    // Internally Parallelised
+    init_pa(n, a, pa, t); // Internally Parallelised
 
     clock_t t_clock; 
     t_clock = clock(); 
-
+    printf("Initialised matrices!\n");
     // k is the column here
     for(int k = 0; k < n; k++){  
-        // Might Confuse indexing
-        // Remember we shifted from 1 indexing in pseudo-code to 0-indexing here
 
-        int k_prime = find_max(a, n, k); // store the row(i) with the max value (pivot) in this column (k)
+        int k_prime = find_max(a, n, k); // Find the pivot
         
-        if(k_prime == -1){
+        if(k_prime == -1){ //Happens when all elements in this column == 0
             printf("Error : Singular Matrix");
             return;
         }
@@ -114,14 +112,23 @@ void LU(int n, int t){
         // Now that pivot has been discovered, start swapping
 
         SWAP(int, pi[k], pi[k_prime]);
-        swap_row(a, k, k_prime, n);
-        swap_row(l, k, k_prime, k);
+        //swap_row(a, k, k_prime, n, t);
+        //swap_row(l, k, k_prime, k, t);
+#pragma omp parallel num_threads(t)
+{
+        swap_row_v2(a[k], a[k_prime], n);
+}
+#pragma omp parallel num_threads(t)
+{
+        swap_row_v2(l[k], l[k_prime], k);
+}
+
         u[k][k] = a[k][k];
 
         // Swaps Completed. Now re-adjust l, u and a appropriately
 
-        upd_UL(a, n, k, u, l);
-        upd_A(a, n, k, u, l);
+        upd_UL_v2(a, n, k, u, l, t);
+        upd_A_v2(a, n, k, u, l, t);
 
         // Complete for the this iteration of k
     }
@@ -135,7 +142,7 @@ void LU(int n, int t){
     // Now use permutation matrix to permute rows of A
     mat_rearrange(pa, pi, n);
     
-    mat_mult(l, u, a_prime, n);  // a_prime = LU
+    mat_mult(l, u, a_prime, n, t);  // a_prime = LU
 /*
     printf("matrix L = \n");
     mat_print(l, n);
@@ -149,7 +156,7 @@ void LU(int n, int t){
     printf("Pi = \n");
     for(int i = 0; i < n; i++){printf("pi[%d] = %d \n",i,  pi[i]);}
 */
-    mat_sub(pa, a_prime, n);      // pa = PA - LU
+    mat_sub(pa, a_prime, n, t);      // pa = PA - LU
 
     // printf("Matrix PA-LU  = \n");
     // mat_print(pa, n);
@@ -160,11 +167,7 @@ void LU(int n, int t){
 
     // Freeing space
     for(int i = 0; i < n; i++){
-        free(u[i]);
-        free(l[i]);
-        free(a_prime[i]);
-        free(a[i]);
-        free(pa[i]);
+        free(u[i]); free(l[i]); free(a_prime[i]); free(a[i]); free(pa[i]);
     }
 
     return;
@@ -236,16 +239,16 @@ void init_l(int n, double** m){
     return;
 }
 
-void init_pi(int n, int* m){
-#pragma parallel for num_threads(t)
+void init_pi(int n, int* m, int t){
+#pragma omp parallel for num_threads(t)
     for (int i = 0; i < n; i++){ // Row
         m[i] = i;
     }
     return;
 }
 
-void init_pa(int n, double** m1, double** m2){
-#pragma parallel for num_threads(t)
+void init_pa(int n, double** m1, double** m2, int t){
+#pragma omp parallel for num_threads(t)
     for(int i = 0; i < n*n; i++) {
         m2[i/n][i%n] = m1[i/n][i%n];
     }
@@ -257,18 +260,25 @@ void init_pa(int n, double** m1, double** m2){
 int find_max(double** a, int n, int k){
     int k_prime = -1;
     double max = 0;
+// #pragma omp parallel for num_threads(t)
     for(int i = k ; i < n; i++){
+        // Parallelization should work, because whatever thread encounters global maxima, it will find if true and set k_prime to i
+        // What is some other thread also has it's if evaluated to true at this point, but just hasn't set the k_prime? It will now overlap and set the k_prime to the non-maxima index
         if(max < fabs(a[i][k])){
+// #pragma omp critical
+// {
             max = fabs(a[i][k]);
             k_prime = i;
+//}
         }
     }
+
     return k_prime;
 }
 
-void swap_row(double** m, int r1, int r2, int k_upto){
+void swap_row(double** m, int r1, int r2, int k_upto, int t){
     
-#pragma parallel for num_threads(t) 
+#pragma omp parallel for num_threads(t) 
     for(int jj = 0; jj < k_upto; jj++){
         double temp = m[r1][jj];
         m[r1][jj] = m[r2][jj];
@@ -278,21 +288,71 @@ void swap_row(double** m, int r1, int r2, int k_upto){
     return;
 }
 
-void upd_UL(double** a, int n, int k, double** u, double** l){
+void swap_row_v2(double* m1, double* m2, int k_upto){
+    int my_rank = omp_get_thread_num();
+    int thread_count = omp_get_num_threads();
 
-#pragma parallel for num_threads(t) 
+    int per_thread = (k_upto+1)/thread_count;
+    int i_start = my_rank * per_thread;
+    int i_end   = i_start + per_thread;
+
+    if(my_rank == thread_count -1){i_end = k_upto;}
+
+    double temp;
+    for (int jj = i_start; jj < i_end; jj++){ // Row
+        temp = m1[jj];
+        m1[jj] = m2[jj];
+        m2[jj] = temp;
+    }
+    return;
+}
+
+
+void upd_UL(double** a, int n, int k, double** u, double** l, int t){
+
+    double x = u[k][k];
+#pragma omp parallel for num_threads(t) 
     for(int i = k+1; i < n; i++){ // k and n don't change, can afford to be shared variables
-        l[i][k] = a[i][k]/u[k][k];
+        l[i][k] = a[i][k]/x;
         u[k][i] = a[k][i];
     }
 
     return;
 }; 
 
+void upd_UL_v2(double** a, int n, int k, double** u, double** l, int t){
 
-void upd_A(double** a, int n, int k, double** u, double** l){
+    double x = u[k][k];
+    int work = n-k-1;
+    if(work == 0){return;}
+    if(work < t){
+        for(int i = k+1; i < n; i++){ 
+            l[i][k] = a[i][k]/x;
+            u[k][i] = a[k][i];
+        }
+        return;
+    }
 
-#pragma parallel for num_threads(t) 
+    int per_thread = work/t;
+    
+#pragma omp parallel for num_threads(t)
+    for(int my_rank = 0; my_rank < t; my_rank++){ 
+        int i_start = k+1+ my_rank * per_thread;
+        int i_end   = i_start + per_thread;
+        if(my_rank == t-1){i_end = n;}
+        for(int i = i_start; i < i_end; i++){
+            l[i][k] = a[i][k]/x;
+            u[k][i] = a[k][i];
+        }
+    }
+
+    return;
+}; 
+
+
+void upd_A(double** a, int n, int k, double** u, double** l, int t){
+
+#pragma omp parallel for num_threads(t) 
     for(int i = k+1; i < n; i++){
         for(int j = k+1; j < n; j++){ // j is declared inside the thread - private
             a[i][j] = a[i][j] - l[i][k]*u[k][j]; // k and n don't change, can afford to be shared variables
@@ -303,10 +363,40 @@ void upd_A(double** a, int n, int k, double** u, double** l){
 }; 
 
 
+void upd_A_v2(double** a, int n, int k, double** u, double** l, int t){
+
+    int work = n-k-1;
+    if(work == 0){return;}
+    if(work < t){
+        for(int i = k+1; i < n; i++){
+            for(int j = k+1; j < n; j++){
+                a[i][j] = a[i][j] - l[i][k]*u[k][j];
+            }
+        }
+        return;
+    }
+
+    int per_thread = work/t;
+    
+#pragma omp parallel for num_threads(t)
+    for(int my_rank = 0; my_rank < t; my_rank++){ 
+        int i_start = k+1+ my_rank * per_thread;
+        int i_end   = i_start + per_thread;
+        if(my_rank == t-1){i_end = n;}
+        for(int i = i_start; i < i_end; i++){
+            for(int j = k+1; j < n; j++){ 
+                a[i][j] = a[i][j] - l[i][k]*u[k][j];
+            }
+        }
+    }
+
+    return;
+}; 
+
 /* Verification Functions */
 
-void mat_mult(double** m1, double** m2, double** m3, int n){
-#pragma parallel for num_threads(t)
+void mat_mult(double** m1, double** m2, double** m3, int n, int t){
+#pragma omp parallel for num_threads(t)
     for(int row = 0; row < n; row++){
         for(int col = 0; col < n; col++){
             double cursum = 0;
@@ -333,8 +423,8 @@ void mat_rearrange(double** m, int* pi, int n){
     return;
 }
 
-void mat_sub(double** m1, double** m2, int n){
-#pragma parallel for num_threads(t)
+void mat_sub(double** m1, double** m2, int n, int t){
+#pragma omp parallel for num_threads(t)
     for(int row = 0; row < n; row++){
         for(int col = 0; col < n; col++){
             m1[row][col] -= m2[row][col];
